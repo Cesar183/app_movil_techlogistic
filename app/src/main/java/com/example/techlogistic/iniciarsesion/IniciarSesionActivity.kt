94cd358d448e6120cdd48dbf4ca0e08d34222488
Cesar <cesar_a_mar_@hotmail.com>
2023-11-07 17:11:04 -0500

    Validacion de inicio de sesion de manera local

--- app/src/main/java/com/example/techlogistic/iniciarsesion/IniciarSesionActivity.kt
+++ app/src/main/java/com/example/techlogistic/iniciarsesion/IniciarSesionActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.example.techlogistic.InicioActivity
 import com.example.techlogistic.R
@@ -14,15 +15,30 @@ class IniciarSesionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iniciar_sesion)
 
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
         val btnIniciarSesion = findViewById<Button>(R.id.btnIniciarSesion)
         btnIniciarSesion.setOnClickListener {
-            Toast.makeText(this, "Iniciando Sesion...", Toast.LENGTH_SHORT).show()
-            dashBoardUsuario()
+
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            if (isValidCredentials(email, password)) {
+                Toast.makeText(this, "Iniciando Sesion...", Toast.LENGTH_SHORT).show()
+                dashBoardUsuario()
+            } else {
+                Toast.makeText(this, "Inicio de sesión fallido!", Toast.LENGTH_SHORT).show()
+            }
+
         }
         val btnVolver = findViewById<Button>(R.id.btnVolverInicio)
         btnVolver.setOnClickListener {
             volverInicio()
         }
+    }
+
+    private fun isValidCredentials(email: String, password: String): Boolean {
+        return (email == "john@correo.com" || email == "doe@correo.com") && (password == "1234" || password == "1234")
     }
 
     private fun dashBoardUsuario() {
